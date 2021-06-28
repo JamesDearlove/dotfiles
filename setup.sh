8f1b98b4aecb0586887ac25c71e8e1550640dccd
James Dearlove <jamesdearlove@outlook.com>
2021-06-29 00:03:44 +1000

    Updates

--- setup.sh
+++ setup.sh
@@ -4,57 +4,72 @@ set -e
 
 LSB_RELEASE=$(lsb_release -cs)
 
-# Dependencies
+# -- APT Sources --
+
+echo "== APT Sources =="
+
+# docker
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+
+echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+$LSB_RELEASE stable" | sudo tee /etc/apt/sources.list.d/docker.list
+
+# azure cli
+curl -fsSL https://packages.microsoft.com/keys/microsoft.asc | sudo gpg --dearmor -o /usr/share/keyrings/microsoft.gpg
+
+echo "deb [arch=amd64 signed-by=/usr/share/keyrings/microsoft.gpg] https://packages.microsoft.com/repos/azure-cli/ \
+$LSB_RELEASE main" | sudo tee /etc/apt/sources.list.d/azure-cli.list
+
+# terraform
+curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/share/keyrings/hashicorp.gpg
+
+echo "deb [arch=amd64 signed-by=/usr/share/keyrings/hashicorp.gpg] https://apt.releases.hashicorp.com \
+$LSB_RELEASE main" > /etc/apt/sources.list.d/hashicorp.list
+
+# -- Dependencies --
+
+echo "== Dependencies =="
+
+sudo apt-get update
+
+sudo apt-get install -y \
+make build-essential libssl-dev zlib1g-dev \ # python build
+libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm \
+libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev \
+apt-transport-https ca-certificates gnupg lsb-release \ # docker & azure cli
+software-properties-common # terraform
+
+# -- Install --
+
+echo "== Installers =="
+
+# docker
+sudo apt-get install docker-ce docker-ce-cli containerd.io
+
+# azure cli
+sudo apt-get install azure-cli
+
+# terraform cli
+sudo apt-get install terraform
 
 # pyenv
-sudo apt-get install make build-essential libssl-dev zlib1g-dev \
-libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm \
-libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev
-
 git clone https://github.com/pyenv/pyenv.git ~/.pyenv
 
 # poetry
 curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py | python -
 
 # nvm nodejs & npm
-
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
 nvm install node
-
-# docker
-sudo apt-get install -y apt-transport-https ca-certificates curl gnupg lsb-release
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
-
-echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
-$LSB_RELEASE stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-
-sudo apt-get update
-sudo apt-get install docker-ce docker-ce-cli containerd.io
-
-# azure cli
-sudo apt-get install ca-certificates curl apt-transport-https lsb-release gnupg
-curl -fsSL https://packages.microsoft.com/keys/microsoft.asc | sudo gpg --dearmor -o /usr/share/keyrings/microsoft.gpg
-
-echo "deb [arch=amd64 signed-by=/usr/share/keyrings/microsoft.gpg] https://packages.microsoft.com/repos/azure-cli/ \
-$LSB_RELEASE main" > /etc/apt/sources.list.d/azure-cli.list
-
-sudo apt-get update
-sudo apt-get install azure-cli
-
-# terraform cli
-
-sudo apt-get install -y gnupg software-properties-common curl
-curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/share/keyrings/hashicorp.gpg
-
-echo "deb [arch=amd64 signed-by=/usr/share/keyrings/hashicorp.gpg] https://apt.releases.hashicorp.com \
-$LSB_RELEASE main" > /etc/apt/sources.list.d/hashicorp.list
-    
-sudo apt-get update 
-sudo apt-get install terraform
 
 # zsh
 sudo apt-get install -y zsh
 
-# Config Files
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# -- Config Files --
+echo "== Copying Configs =="
 cp ./.zshrc ~
 cp ./.zprofile ~
+
+echo "== Done =="
