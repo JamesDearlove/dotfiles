8d3e3252143fe0b43824a0fdf65e79d024c47d28
James Dearlove <jamesdearlove@outlook.com>
2021-11-14 12:39:02 +1000

    Realising shortcuts exist

--- WindowsStartup.cmd
+++ WindowsStartup.cmd
@@ -1,4 +1,9 @@
 @echo off
 REM List of scripts that run on startup
 
+echo Jimmy's Startup Script
+echo.
+
 powershell .\SpotlightCopy.ps1
+
+echo Done!
--- WindowsStartupFolder.cmd
+++
@@ -1,5 +0,0 @@
-@echo off
-REM Redirect to WindowsStartup.cmd file
-
-cd C:\Users\james\Repos\dotfiles\
-WindowsStartup.cmd
