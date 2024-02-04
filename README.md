261fad97a56361bec8ba2e9619abfeef49d8f9d2
James Dearlove <jamesdearlove@outlook.com>
2024-02-04 23:44:56 +1000

    Windows setup script

--- README.md
+++ README.md
@@ -1,3 +1,23 @@
 # Jimmy's Dot Files
 
 Git repo store for all the configs I might need
+
+## Setup Scripts
+
+I have setup scripts for various operating systems, usually to setup the environments I run under them.
+
+### Windows
+
+This sets up a basic Windows install with Firefox, VSCode, and a couple of developer tools.
+
+Run a Windows PowerShell (built in v5.1, not v7) as admin, and run the `windows-setup.ps1` script.
+
+### Mac
+
+In progress, haven't setup that many Macs recently to need it.
+
+### Ubuntu
+
+This script was originally designed for my Ubuntu WSL environment for all dev work. It contains various tools and CLIs I've needed for work and shell customistations.
+
+Run a terminal and run the `setup.sh` script. It will require sudo access.
---
+++ windows-setup.ps1
@@ -0,0 +1,48 @@
+# Jimmy's simple Windows setup.
+#Requires -RunAsAdministrator
+
+$WinVersion = [System.Environment]::OSVersion.Version
+
+# Force add winget if it's not already available, but only if on default Windows PowerShell
+if ( $PSVersionTable.PSVersion.Major -eq 5 )
+{
+    Write-Output "Making sure winget is available"
+    Add-AppxPackage -RegisterByFamilyName -MainPackage Microsoft.DesktopAppInstaller_8wekyb3d8bbwe
+}
+
+$wingetpkgs = @(
+    '9N0DX20HK701' # Terminal (Store edition)
+    'Git.Git'
+    '7zip.7zip'
+    'Mozilla.Firefox'
+    'Microsoft.PowerShell'
+    'Microsoft.VisualStudioCode'
+)
+
+foreach ( $app in $wingetpkgs )
+{
+    Write-Output "Installing package: $app"
+    winget install --id=$app -e -h --silent --accept-package-agreements
+}
+
+# PowerShell profile
+# TODO: Either install OhMyPOSH or have a clean version without.
+# copy ./powershell_profile.ps1 $PROFILE
+
+# Force layout of taskbar
+Write-Output "Now for some agressive customisation measures"
+Import-StartLayout -LayoutPath ".\windows\StartLayout.xml" -MountPath $env:SystemDrive\
+
+# Taskbar defaults (Search off, news off)
+Set-ItemProperty -Path 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Search' -Name 'SearchboxTaskbarMode' -Value 0 -Force
+Set-ItemProperty -Path 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Feeds' -Name 'ShellFeedsTaskbarViewMode' -Value 2 -Force
+
+# Widgets off, chat off, left aligned for Win 11.
+if ($WinVersion.build -ge 22000) {
+    Set-ItemProperty -Path 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced' -Name 'TaskbarDa' -Value 0 -Force
+    Set-ItemProperty -Path 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced' -Name 'TaskbarMn' -Value 0 -Force
+    Set-ItemProperty -Path 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced' -Name 'TaskbarAl' -Value 0 -Force
+}
+
+# Show file extensions
+Set-ItemProperty -Path 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced' -Name 'HideFileExt' -Value 0 -Force
