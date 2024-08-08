fe3dba1c775fd8fd207c3eb77ae307fa534571c3
James Dearlove <james.dearlove@des.qld.gov.au>
2024-08-08 12:19:08 +1000

    Some more defaults

--- macos-setup.sh
+++ macos-setup.sh
@@ -10,8 +10,14 @@ softwareupdate --install-rosetta --agree-to-license
 # Install Brew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
-brew install --cask iterm2 visual-studio-code
+brew install --cask iterm2 visual-studio-code raycast
 
-brew install nvm
+brew install nvm displayplacer
+
+# Powerlevel10k
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 
 
+
+# Defaults pls
+defaults write -g ApplePressAndHoldEnabled -bool false
